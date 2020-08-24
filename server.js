418e6d29e6359341b616ed150d9c9bab6b608054
letamin <lethanhminh0901@gmail.com>
2020-08-24 16:45:07 +0700

    CORS allow

--- server.js
+++ server.js
@@ -4,6 +4,16 @@ const app = express();
 const config = require('./config/index')
 const port = process.env.PORT || config.port
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, token"
+    );
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH");
+    next();
+});
+
 mongoose.connect(config.mongoUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true
