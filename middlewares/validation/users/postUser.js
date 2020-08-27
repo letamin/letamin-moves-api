d2a231b50160cad195d76472d548bd89a3ee7edf
letamin <lethanhminh0901@gmail.com>
2020-08-28 02:44:10 +0700

    testing postUser

--- middlewares/validation/users/postUser.js
+++ middlewares/validation/users/postUser.js
@@ -3,7 +3,7 @@ const { User } = require('../../../models/User');
 
 //Validate users creation
 const validatePostUser = async (req, res, next) => {
-    let errors = {};
+    let errors = [];
 
     const email = req.body.email || '';
     const password = req.body.password || '';
@@ -11,30 +11,30 @@ const validatePostUser = async (req, res, next) => {
 
     //Validate email
     if (validator.isEmpty(email)) {
-        errors.email = "Email is required";
+        errors.push = "Email is required";
     } else if (!validator.isEmail(email)) {
-        errors.email = "Email is invalid.";
+        errors.push = "Email is invalid.";
     } else {
         const user = await User.findOne({ email });
         if (user) {
-            errors.email = "Email is already existed."
+            errors.push = "Email is already existed."
         }
     }
 
     //Validate password
     if (validator.isEmpty(password)) {
-        errors.password = "Password is required";
+        errors.push = "Password is required";
     } else if (!validator.isLength(password, { min: 6 })) {
-        errors.password = "Password must be at least 6 characters.";
+        errors.push = "Password must be at least 6 characters.";
     }
 
     //validate full name
     if (validator.isEmpty(fullName)) {
-        errors.fullName = "Name is required";
+        errors.push = "Name is required";
     }
 
     if (Object.keys(errors).length === 0 && errors.constructor === Object) return next();
-    return res.status(400).json({ message: errors });
+    return res.status(400).json(errors);
 }
 
 module.exports = {
