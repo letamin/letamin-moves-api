df815e1c1e0fadb5d748a973ed5b2b3dced7b906
letamin <lethanhminh0901@gmail.com>
2020-09-03 15:51:59 +0700

    test contact

--- routes/api/controllers/contacts/index.js
+++ routes/api/controllers/contacts/index.js
@@ -3,8 +3,11 @@ const contactController = require('./contact.controller');
 const router = express.Router();
 const { validateContact } = require('../../../../middlewares/validation/contacts/postContact');
 const { authenticate, authorize } = require('../../../../middlewares/auth/index');
+const { model } = require('mongoose');
 
 router.get('/', authenticate, authorize(['admin']), contactController.getContacts);
 router.get('/:id', authenticate, authorize(['admin']), contactController.getContactById);
 router.post('/', authenticate, validateContact, contactController.postContact);
 router.delete('/:id', authenticate, authorize(['admin']), contactController.deleteContactById);
+
+module.exports = router;
